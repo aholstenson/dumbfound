0b66118c103fa81f13b4e485d20877e6b11ec283
Andreas Holstenson <a@holstenson.se>
2019-04-16 20:53:11 +0200

    feat(tools): Updating block generator with Markdown list generation

--- tools/unicode-block-generator.js
+++ tools/unicode-block-generator.js
@@ -4,16 +4,25 @@ const fs = require('fs');
 
 const block = fs.readFileSync('./blocks.txt').toString();
 
+const names = [];
 for(const line of block.split('\n')) {
 	const m = line.match(/^([0-9A-F]+)\.\.([0-9A-F]+); (.+)/);
 	if(! m) continue;
 
 	const lower = m[1];
 	const upper = m[2];
-	const name = m[3]
+	const name = 'unicode' + m[3]
 		.replace('-', '')
 		.replace(/ [A-Za-z]/g, f => f.trim().toUpperCase());
 
 	console.log('/** Unicode Block: ' + m[3] + ' */');
-	console.log('export const unicode' + name + ' = new CodepointRangeGenerator(0x' + lower + ', 0x' + upper + ');');
+	console.log('export const ' + name + ' = new CodepointRangeGenerator(0x' + lower + ', 0x' + upper + ');');
+
+	names.push(name);
+}
+
+console.log();
+console.log('Markdown list of blocks:');
+for(const name of names) {
+	console.log('* `' + name + '`');
 }
